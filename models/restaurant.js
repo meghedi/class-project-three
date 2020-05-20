7df0fedf54d87c4fa5a9598979c29e91d98edb47
Meghedi Hacopi <hacopian.m@gmail.com>
2020-05-19 20:48:23 -0700

    add save process

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const restaurantSchema = new Schema({
-  title: { type: String, required: true },
-  authors: { type: [], required: true },
-  description: String,
+  restaurant: { type: String, required: true },
+  cuisines: { type: [], required: true },
+  rating: {type: Number},
+  location: String,
   image : String,
   link: String
 });
-
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
-
 module.exports = Restaurant;
