71b75762a14d4e7755ac3ff34bc432380d32b978
Americ Ngwije <ngwijea@yahoo.com>
2020-05-23 13:21:11 -0700

    updated card style

--- client/src/components/Media.js
+++ client/src/components/Media.js
@@ -1,42 +1,68 @@
 import React from "react";
-import{
+import {
   Card,
   CardImg,
+  CardTitle,
   CardBody,
-  CardText
+  CardLink,
+  CardText,
+  Row,
+  Col,
 } from "reactstrap";
 
-const styles = {
-  buttonStyle: {
-    position: "absolute",
-    top: 5,
-    right: 5,
-  },
-  mediaStyle: {
-    position: "relative",
-  },
-};
+// const styles = {
+//   buttonStyle: {
+//     position: "absolute",
+//     top: 5,
+//     right: 5,
+//   },
+//   mediaStyle: {
+//     position: "relative",
+//   },
+// };
 function Media({ restaurant, cuisines, rating, image, link, handleSave }) {
   return (
     <>
       <div className="section">
-              <Card style={{ width: "40rem" }}>
-                <CardImg alt={restaurant} src={image} top style={{width:"320px", height:"320px"}}></CardImg>
-                <CardBody style={{ position: "relative" }}>
-                  <CardText>
-                    <b>{restaurant}</b>
-                  </CardText>
-                  <button
-                    type="button"
-                    id="btnSave"
-                    style={styles.buttonStyle}
-                    className="btn btn-default"
-                    onClick={handleSave}
-                  >
-                    Save
-                  </button>
-                </CardBody>
-              </Card>
+        {/* <Card style={{ width: "40rem" }}> */}
+        <Card className="mb-3" body outline color="primary">
+          <CardTitle>
+            <h1>{restaurant}</h1>
+          </CardTitle>
+          <CardImg
+            alt={restaurant}
+            src={image}
+            className="card-img-top img-thumbnail"
+            top
+            // style={{ width: "320px", height: "320px" }}
+          ></CardImg>
+
+          <CardBody style={{ position: "relative" }}>
+            <CardText>
+              <ul class="list-group list-group-flush center">
+                <li class="list-group-item">Rating:</li>
+                <li class="list-group-item">Cuisines:</li>
+                <li class="list-group-item">Phone:</li>
+                <li class="list-group-item">Address</li>
+                <a href="{link}" class="list-group-item list-group-item-action">
+                  Yelp Listing
+                </a>
+              </ul>
+              <button
+              type="button"
+              id="btnSave"
+              // style={styles.buttonStyle}
+              className="btn btn-primary btn-lg active"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+            </CardText>
+            <CardLink href="#">{link}</CardLink>
+          </CardBody>
+
+        </Card>
+
       </div>
     </>
   );
