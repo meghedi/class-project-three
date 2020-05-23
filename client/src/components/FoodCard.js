5e059e0cf0a71f08a6cead46db78f65dfaed3cbe
Meghedi Hacopi <hacopian.m@gmail.com>
2020-05-23 13:19:28 -0700

    fixed slowness and added zipcode search

--- client/src/components/FoodCard.js
+++ client/src/components/FoodCard.js
@@ -19,12 +19,12 @@ const buttonStyle={
 
 function FoodCard(props) {
   const searchItem = [
-    { term: "sushi", location: "91206", rating: "5", limit: 5 },
-    { term: "tacos", location: "91206", rating: "5", limit: 5 },
-    { term: "burger", location: "91206", rating: "5", limit: 5 },
-    { term: "indian", location: "91206", rating: "5", limit: 5 },
-    { term: "pho", location: "91206", rating: "5", limit: 5 },
-    { term: "icecream", location: "91206", rating: "5", limit: 5 },
+    { term: "sushi", location: "Los Angeles, CA", rating: "5", limit: 5, id:"F1" },
+    { term: "tacos", location: "Los Angeles, CA", rating: "5", limit: 5 , id:"F"},
+    { term: "burger", location: "Los Angeles, CA", rating: "5", limit: 5 , id:"F3"},
+    { term: "indian", location: "Los Angeles, CA", rating: "5", limit: 5 , id:"F4"},
+    { term: "pho", location: "Los Angeles, CA", rating: "5", limit: 5 , id:"F5"},
+    { term: "icecream", location: "Los Angeles, CA", rating: "5", limit: 5, id:"F6" },
   ];
   return (
     <>
@@ -33,8 +33,7 @@ function FoodCard(props) {
           <Row>
             <Col className="ml-auto mr-auto" md="4">
               {searchItem.slice(0, 2).map((item) => (
-                <>
-                <Card style={{ width: "20rem" }}>
+                <Card style={{ width: "20rem" }} key={item.id}>
                   <CardImg
                     alt="..."
                     src={require(`../assets/img/${item.term}.jpg`)}
@@ -51,15 +50,13 @@ function FoodCard(props) {
                     </CardText>
                   </CardBody>
                 </Card>
-                <hr/>
-                </>
+
               ))}
             </Col>
             <hr />
             <Col className="ml-auto mr-auto" md="4">
               {searchItem.slice(2, 4).map((item) => (
-                <>
-                  <Card style={{ width: "20rem" }}>
+                  <Card style={{ width: "20rem" }} key={item.id}>
                     <CardImg
                       alt="..."
                       src={require(`../assets/img/${item.term}.jpg`)}
@@ -76,15 +73,12 @@ function FoodCard(props) {
                       </CardText>
                     </CardBody>
                   </Card>
-                  <hr />
-                </>
               ))}
             </Col>
             <hr />
             <Col className="ml-auto mr-auto" md="4">
               {searchItem.slice(4, 6).map((item) => (
-                <>
-                  <Card style={{ width: "20rem" }}>
+                  <Card style={{ width: "20rem" }} key={item.id}>
                     <CardImg
                       alt="..."
                       src={require(`../assets/img/${item.term}.jpg`)}
@@ -101,8 +95,7 @@ function FoodCard(props) {
                       </CardText>
                     </CardBody>
                   </Card>
-                  <hr />
-                </>
+
               ))}
             </Col>
             <hr />
